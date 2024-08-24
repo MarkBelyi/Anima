ecb2304846c8af6a4e906b048d52380a19f778b5
MarkBelyi <123000957+MarkBelyi@users.noreply.github.com>
2024-08-24 21:10:55 +0500

    Add Color for project

---
+++ lib/Color/colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary Colors
+  static const Color primaryGreenLight = Color(0xFF8BD352);
+  static const Color primaryGreen = Color(0xFF81BC53);
+  static const Color primaryGreenDark = Color(0xFFBAD7A3);
+  static const Color primaryLight = Color(0xFFF2F1F9);
+
+  // Neutral Colors
+  static const Color neutralDark = Color(0xFF343434);
+  static const Color neutralGrayDark = Color(0xFF898989);
+  static const Color neutralGray = Color(0xFF989898);
+  static const Color neutralGrayLight = Color(0xFFCACACA);
+  static const Color neutralLight = Color(0xFFE0E0E0);
+  static const Color neutralWhite = Color(0xFFFFFFFF);
+
+  // Semantic Colors
+  static const Color semanticRed = Color(0xFFFF4267);
+  static const Color semanticBlue = Color(0xFF0890FE);
+  static const Color semanticYellow = Color(0xFFFFAF2A);
+  static const Color semanticGreen = Color(0xFF52D5BA);
+  static const Color semanticOrange = Color(0xFFFB6B18);
+}
