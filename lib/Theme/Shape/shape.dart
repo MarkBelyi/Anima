5a25d6c3031a92980ddafb80dfae6d0a2e245097
MarkBelyi <123000957+MarkBelyi@users.noreply.github.com>
2024-08-26 23:40:15 +0500

    Project structure huge update #2

---
+++ lib/Theme/Shape/shape.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppShape{
+
+  static const BorderRadius topRoundedCorners = BorderRadius.only(
+    topLeft: Radius.circular(30.0),
+    topRight: Radius.circular(30.0),
+  );
+
+}
